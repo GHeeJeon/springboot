5967101557062289fe03fa6722aca1deccf415cc
GHeeJeon <jhjeon_99@naver.com>
2022-09-25 00:07:10 +0900

    main-update code

--- spring/src/main/java/com/example/spring/dao/CustomerRepository.java
+++ spring/src/main/java/com/example/spring/dao/CustomerRepository.java
@@ -10,11 +10,10 @@ public interface CustomerRepository extends CrudRepository<Customer, Integer> {
     List<Customer> findByName(String name);
     List<Customer> findByNameLike(String name);
     List<Customer> findByNameLikeOrderByAddressAsc(String name);
-
     List<Customer> findByNameOrAddress(String name, String address);
 
-    @Query("from Customer where name = ?1 and address = ?2")
-    List<Customer> findVipList(String value1, String value2);
+    // @Query("from Customer where name = ?1 and address = ?2")
+    // List<Customer> findVipList(String value1, String value2);
     @Query(value = "select * from Customer where name = ?1 and address = ?2", nativeQuery = true)
     List<Customer> findVipList(String value1, String value2);
 
